f04aaf9bf7b000c7c777232a6db286f80780401e
aquila62 <aquila62@users.noreply.github.com>
2016-08-26 09:36:00 -0500

    Add files via upload

---
+++ deinst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make -f lfsr15.mak clean
+make -f sv.mak clean
+make -f twr.mak clean
+make -f crc.mak clean
+make -f etausgen.mak clean
+make -f etausraw.mak clean
+make -f lmt.mak clean
+make -f getprm.mak clean
---
+++ inst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make -f lfsr15.mak
+make -f sv.mak
+make -f twr.mak
+make -f crc.mak
+make -f etausgen.mak
+make -f etausraw.mak
+make -f lmt.mak
+make -f getprm.mak
