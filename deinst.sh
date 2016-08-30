7e4404c5a6bb20b3ba36ee334e297b145bec49cb
aquila62 <aquila62@users.noreply.github.com>
2016-08-30 13:58:23 -0500

    Changed CRC compile procedures.

--- deinst.sh
+++ deinst.sh
@@ -2,12 +2,14 @@
 make -f lfsr15.mak clean
 make -f sv.mak clean
 make -f twr.mak clean
-make -f crc.mak clean
 make -f etausgen.mak clean
 make -f etausraw.mak clean
 make -f lmt.mak clean
 make -f getprm.mak clean
-make -f crctbl.mak clean
-make -f crc16.mak clean
 make -f putnine.mak clean
+make -f crctbl16.mak clean
+make -f crctbl32.mak clean
+make -f crc.mak clean
+make -f crc16.mak clean
 rm -f crc16tbl.inc
+rm -f crc32tbl.inc
--- inst.sh
+++ inst.sh
@@ -2,26 +2,44 @@
 make -f lfsr15.mak
 make -f sv.mak
 make -f twr.mak
-make -f crc.mak
 make -f etausgen.mak
 make -f etausraw.mak
 make -f lmt.mak
 make -f getprm.mak
+make -f putnine.mak
 #----------------------------
-make -f crctbl.mak
-if [ -f crctbl ]
+make -f crctbl32.mak
+if [ -f crctbl32 ]
 then
-bldtbl.sh
+bldtbl32.sh
 else
-echo "crctbl.asm did not compile"
-echo "Can not run bldtbl.sh"
+echo "crctbl32.asm did not compile"
+echo "Can not run bldtbl32.sh"
+exit 1
+fi
+#----------------------------
+if [ -f crc32tbl.inc ]
+then
+make -f crc.mak
+else
+echo "crc32.asm did not compile"
+echo "Could not include crc32tbl.inc"
+exit 1
+fi
+#----------------------------
+make -f crctbl16.mak
+if [ -f crctbl16 ]
+then
+bldtbl16.sh
+else
+echo "crctbl16.asm did not compile"
+echo "Can not run bldtbl16.sh"
 exit 1
 fi
 #----------------------------
 if [ -f crc16tbl.inc ]
 then
 make -f crc16.mak
-make -f putnine.mak
 else
 echo "crc16.asm did not compile"
 echo "Could not include crc16tbl.inc"
